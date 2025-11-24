a3b7552aac13aea5f1b4d680e9205ea389f4f686
shuheiintokyo <92214102+shuheiintokyo@users.noreply.github.com>
2025-11-24 10:58:19 +0900

    update

---
+++ eitangos/eitangosApp.swift
@@ -0,0 +1,21 @@
+//
+//  eitangosApp.swift
+//  eitangos
+//
+//  Created by Shuhei Kinugasa on 2025/11/24.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct eitangosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ eitangosTests/eitangosTests.swift
@@ -0,0 +1,17 @@
+//
+//  eitangosTests.swift
+//  eitangosTests
+//
+//  Created by Shuhei Kinugasa on 2025/11/24.
+//
+
+import Testing
+@testable import eitangos
+
+struct eitangosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
